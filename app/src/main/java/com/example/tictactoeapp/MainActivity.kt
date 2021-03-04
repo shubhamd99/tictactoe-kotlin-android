7dd86fb03906f6d901621199ad09f0b1ccdab6ab
Shubham Dhage <shubham.dhage@swiggy.in>
2021-03-04 08:45:58 +0530

    add button onClick event

--- app/src/main/java/com/example/tictactoeapp/MainActivity.kt
+++ app/src/main/java/com/example/tictactoeapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.tictactoeapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buClick(view: View) {
+        val buSelected = view as Button
+        var cellId = 0
+
+        when(buSelected.id) {
+            R.id.bu1 -> cellId = 1
+            R.id.bu2 -> cellId = 2
+            R.id.bu3 -> cellId = 3
+            R.id.bu4 -> cellId = 4
+            R.id.bu5 -> cellId = 5
+            R.id.bu6 -> cellId = 6
+            R.id.bu7 -> cellId = 7
+            R.id.bu8 -> cellId = 8
+            R.id.bu9 -> cellId = 9
+        }
+
+        Log.d("buClick", buSelected.id.toString())
+        Log.d("cellId", cellId.toString())
     }
 }
