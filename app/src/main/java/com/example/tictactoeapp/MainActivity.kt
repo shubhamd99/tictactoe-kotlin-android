4aaff27be85cfbaea7ca3a0460f2dc3776e08dcd
Shubham Dhage <shubham.dhage@swiggy.in>
2021-03-04 09:18:04 +0530

    add check winner functionality

--- app/src/main/java/com/example/tictactoeapp/MainActivity.kt
+++ app/src/main/java/com/example/tictactoeapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,10 +14,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun buClick(view: View) {
-        val buSelected = view as Button
+        val buSelected = view as Button // Casting
         var cellId = 0
 
         when(buSelected.id) {
+            // R -> Resources
             R.id.bu1 -> cellId = 1
             R.id.bu2 -> cellId = 2
             R.id.bu3 -> cellId = 3
@@ -30,5 +32,84 @@ class MainActivity : AppCompatActivity() {
 
         Log.d("buClick", buSelected.id.toString())
         Log.d("cellId", cellId.toString())
+
+        playGame(cellId, buSelected)
+    }
+
+    var activePlayer = 1
+    var player1 = ArrayList<Int>()
+    var player2 = ArrayList<Int>()
+
+    private fun playGame(cellId: Int, buSelected: Button) {
+        if (activePlayer == 1) {
+            buSelected.text = "X"
+            buSelected.setBackgroundResource(R.color.blue)
+            player1.add(cellId)
+            // change the player after selection
+            activePlayer = 2
+        } else {
+            buSelected.text = "O"
+            buSelected.setBackgroundResource(R.color.darkGreen)
+            player2.add(cellId)
+            // change the player after selection
+            activePlayer = 1
+        }
+
+        // Will disable the button so that user cannot click it again
+        buSelected.isEnabled = false
+
+        checkWinner()
+    }
+
+    private fun checkWinner() {
+        var winner = -1
+
+        // row 1
+        if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
+            winner = 1
+        } else if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
+            winner = 2
+        }
+
+        // row 2
+        if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
+            winner = 1
+        } else if (player2.contains(4) && player2.contains(5) && player2.contains(6)) {
+            winner = 2
+        }
+
+        // row 3
+        if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
+            winner = 1
+        } else if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
+            winner = 2
+        }
+
+        // col 1
+        if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
+            winner = 1
+        } else if (player2.contains(1) && player2.contains(4) && player2.contains(7)) {
+            winner = 2
+        }
+
+        // col 2
+        if (player1.contains(2) && player1.contains(5) && player1.contains(8)) {
+            winner = 1
+        } else if (player2.contains(2) && player2.contains(5) && player2.contains(8)) {
+            winner = 2
+        }
+
+        // col 3
+        if (player1.contains(3) && player1.contains(6) && player1.contains(9)) {
+            winner = 1
+        } else if (player2.contains(3) && player2.contains(6) && player2.contains(9)) {
+            winner = 2
+        }
+
+        if (winner == 1) {
+            Toast.makeText(this, "Player 1 win the game", Toast.LENGTH_LONG).show()
+        } else if (winner == 2) {
+            Toast.makeText(this, "Player 2 win the game", Toast.LENGTH_LONG).show()
+        }
     }
 }
